d087d40fde9d3eeb75144ae4cfa016a3711ad042
mocamocaland <noi_kaas@yahoo.co.jp>
2018-04-02 02:23:59 +0900

    models.py　を編集

--- project/videos/models.py
+++ project/videos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    """moving image"""
+
+    title = models.CharField('動画タイトル', max_length=255)
+    descritption = models.TextField('説明(空欄可)', blank=True)
+    thumbnail = models.ImageField('サムネイル', )
