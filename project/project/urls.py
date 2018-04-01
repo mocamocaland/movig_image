5743aff4513dc9586bf8c1e8ebbe3e9bbc89b184
mocamocaland <noi_kaas@yahoo.co.jp>
2018-04-02 02:08:02 +0900

    startproject側のファイルを編集

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('videos.urls')),
 ]
+
+
+# 開発環境時のみ、Djangoアプリケーション側でメディアファイルを配信
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
