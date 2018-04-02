f4d563bde8563b946a58293da182c080fd3d58a4
mocamocaland <noi_kaas@yahoo.co.jp>
2018-04-02 21:12:43 +0900

    add templates and forms.py

--- project/videos/forms.py
+++ project/videos/forms.py
@@ -8,3 +8,14 @@ class VideoCreateForm(forms.ModelForm):
         model = Video
         fields = ('title', 'description', 'thumbnail', 'upload')
 
+        widgets = {
+            'title': forms.TextInput(attr={ # <input type="text" class="form_control"
+                'class': 'form-control',
+            }),
+            'description': forms.Textarea(attrs={ # <textarea class="form-control"
+                'class': "form-control-file",
+            }),
+            'upload': forms.ClearableFileInput(attrs={
+                'class': "form-control-file",
+            }),
+        }
